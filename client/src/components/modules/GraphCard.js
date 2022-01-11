1377011c72dc57da073cfb9224c6b503789fe6be
Grace Jiang <gracejiang@dhcp-10-29-103-27.dyn.mit.edu>
2022-01-11 10:07:06 -0500

    xddx desmos graph

--- client/src/components/modules/GraphCard.js
+++ client/src/components/modules/GraphCard.js
@@ -1,24 +1,16 @@
 import React, { useState, useEffect } from "react";
-import Desmos from "desmos";
 
 import "./GraphCard.css";
-
 import { get } from "../../utilities";
 
 
 
 const GraphCard = () => {
-    const elt = document.createElement('div');
-    elt.style.width = '600px';
-    elt.style.height = '400px';
-    elt.style.position = 'relative';
-    const calculator = Desmos.GraphingCalculator(elt);
-    calculator.setExpression({ id: 'graph1', latex: 'y=x^2' });
      
     return(
         
-        <div className="">
-            {document.body.append(elt)}
+        <div className="GraphCard-container">
+            <iframe src="https://www.desmos.com/calculator/g7izucn6nn" className="GraphCard-graph"></iframe>
         </div>
     );
 };
