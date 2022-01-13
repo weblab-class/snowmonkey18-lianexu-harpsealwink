2d6ce4dcb89384ef85fc77c97eebf7aeccb484d2
snowmonkey18 <snowmonkey.gt@gmail.com>
2022-01-12 21:52:00 -0500

    add profile page and restyle nav bar

---
+++ client/src/components/pages/Profile.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+import { get } from "../../utilities";
+
+
+const Profile = () => {
+    return(
+        <div>
+            <h1>
+                profile
+            </h1>
+            <p>
+                I am a ninja 
+            </p>
+        </div>
+    );
+};
+
+export default Profile;
