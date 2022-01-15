18077c0d8cef1d7e43f6d6a70a916cec02c05bf3
snowmonkey18 <snowmonkey.gt@gmail.com>
2022-01-15 00:10:24 -0500

    what is happening

--- client/src/components/modules/GraphCard2.js
+++ client/src/components/modules/GraphCard2.js
@@ -1,31 +1,51 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
  
 import JXGBoard from 'jsxgraph-react-js'
- 
-let logicJS = (brd) => {
-  brd.suspendUpdate();
-  var a = brd.create('slider', [[2, 8], [6, 8], [0, 3, 6]], { name: 'a' });
-  var b = brd.create('slider', [[2, 7], [6, 7], [0, 2, 6]], { name: 'b' });
-  var A = brd.create('slider', [[2, 6], [6, 6], [0, 3, 6]], { name: 'A' });
-  var B = brd.create('slider', [[2, 5], [6, 5], [0, 3, 6]], { name: 'B' });
-  var delta = brd.create('slider', [[2, 4], [6, 4], [0, 0, Math.PI]], { name: '&delta;' });
- 
-  var c = brd.create('curve', [
-    function (t) { return A.Value() * Math.sin(a.Value() * t + delta.Value()); },
-    function (t) { return B.Value() * Math.sin(b.Value() * t); },
-    0, 2 * Math.PI], { strokeColor: '#aa2233', strokeWidth: 3 });
-  brd.unsuspendUpdate();
-}
+
  
 const GraphCard2 = (props) => {
+  const [a, seta] = useState(2);
+  const [A, setA] = useState(1);
+  const [b, setb] = useState(3);
+  const [B, setB] = useState(4);
+  const [delta, setdelta] = useState(5);
+
+  const [logicJS, setLogicJS] = useState(0);
+
+
+  useEffect(() => {
+    setLogicJS((brd) => {
+      brd.suspendUpdate();
+     
+      let c = brd.create('curve', [
+        ( t => { return A * Math.sin(a * t + delta); } ),
+        ( t => { return B * Math.sin(b * t); } ),
+        0, 2 * Math.PI], { strokeColor: '#aa2233', strokeWidth: 3 });
+      brd.unsuspendUpdate();
+    })
+
+  }, [a, A, b, B, delta]);
+
+
+  useEffect(() => {
+    console.log(a);
+  }, [a]);
   return (
-    <JXGBoard
+    <>
+    <input type="range" min="1" max="10" value={a} class="slider" onChange={(e) => seta(e.target.value)}/>
+    <input type="range" min="1" max="10" value={b} class="slider" onChange={(e) => setb(e.target.value)}/>
+    <input type="range" min="1" max="10" value={A} class="slider" onChange={(e) => setA(e.target.value)}/>
+    <input type="range" min="1" max="10" value={B} class="slider"onChange={(e) => setB(e.target.value)} />
+    <input type="range" min="1" max="10" value={delta} class="slider" onChange={(e) => setdelta(e.target.value)}/>
+    {logicJS === 0 ? <div/> : <JXGBoard
       logic={logicJS}
       boardAttributes={{ axis: true, boundingbox: [-12, 10, 12, -10] }}
       style={{
         border: "3px solid red"
       }}
-    />
+    />}
+    
+    </>
   )
 }
 
--- client/src/components/pages/Training.js
+++ client/src/components/pages/Training.js
@@ -28,8 +28,63 @@ const Training = (props) => {
 
                 </div>
             </div>
+
             <GraphCard2/>
-            
+
+{/* 
+            <div style="margin:5px">
+                <p>
+                    <label for="c1">
+                        c1:
+                    </label>
+                    <input 
+                        type="range" id="c1" style="border:0; color:#f6931f; font-weight:bold;" 
+                        min="0" max="100" value="60" 
+                        oninput="c1 = this.value*0.01; board.update();" 
+                    />
+                    <label for="f1">
+                        f1:
+                    </label>
+                    <input 
+                        type="range" id="f1" style="border:0; color:#f6931f; font-weight:bold;" 
+                        min="1" max="100" value="7"
+                        oninput="f1 = this.value; board.update();" 
+                    />
+                    <label for="c2">
+                        c2:
+                    </label>
+                    <input 
+                        type="range" id="c2" style="border:0; color:#f6931f; font-weight:bold;" 
+                        min="0" max="100" value="0"
+                        oninput="c2 = this.value*0.01; 
+                                board.updateQuality = board.BOARD_QUALITY_HIGH;
+                                board.update();" 
+                    />
+                    <label for="f2">
+                        f2:
+                    </label>
+                    <input 
+                        type="range" id="f2" style="border:0; color:#f6931f; font-weight:bold;" 
+                        min="1" max="100" value="17"
+                        oninput="f2 = this.value; board.update();" 
+                    />
+                </p>
+            </div>
+
+            <jsxgraph width="500" height="500" box="jxgbox">
+                board = JXG.JSXGraph.initBoard('jxgbox', {boundingbox: ([-2.5,2.5,2.5,-2.5], keepaspectratio:true)});
+                let c1 = 0.6;
+                let c2 = 0.0; 
+                let f1 = 7; 
+                let f2 = 17;
+                let c = board.create('curve', [
+                    ((t) => { Math.cos(t)+ c1*Math.cos(f1*t)+ c2*Math.cos(f2*t) }),
+                    ((t) => { Math.sin(t)+ c1*Math.sin(f1*t)+ c2*Math.sin(f2*t) }),
+                    0,2.02*Math.PI], 
+                    strokeWidth: 2);
+            </jsxgraph>            
+                             */}
+
         </div>
     );
 };
