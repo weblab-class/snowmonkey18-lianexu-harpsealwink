08a6b416bc4fae18c9cb6c2e441b53fbbbd09776
Grace Jiang <gracejiang@dhcp-10-31-91-80.dyn.mit.edu>
2022-01-23 16:46:23 -0500

    about page updated

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./About.css";
+import stretch_ninja from "../modules/ninjas/stretch_ninja.png";
+import sidestep_ninja from "../modules/ninjas/sidestep_ninja.png";
+import levitate_ninja from "../modules/ninjas/levitate_ninja.png";
 
 
 const About = () => {
@@ -16,16 +19,23 @@ const About = () => {
             </h1>
             <div className="About-info">
             <p className="About-typewriter">
-                Train to be a quadratic ninja!
+                 Train to be a quadratic ninja!
             </p>
             <p>
                 This website was designed to teach children all about quadratic graphs 
                 through a series of "Ninja Quests" (aka Training levels), where the constants used to shift a 
                 quadratic graph left/right, to shift the graph up/down, and to stretch the graph
-                are elements that each young ninja must learn to conquer in the quests.
+                are elements that each young ninja must learn to conquer in quests.
             </p>
-            <div className="About-equ"> y=A(x+B)^2+C</div>
-            <div className="About-elements"/>
+            <div className="About-equ"> y = a (x + b)² + c</div>
+            <div className="About-elements">
+                <img src={stretch_ninja} className="About-stretch"/>
+                <div className="About-element"> element a </div>
+                <img src={sidestep_ninja} className="About-sidestep"/>
+                <div className="About-element"> element b </div>
+                <img src={levitate_ninja} className="About-levitate"/>
+                <div className="About-element"> element c </div>
+            </div>
             </div>
         </div>
     );
