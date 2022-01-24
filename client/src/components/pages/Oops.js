d4cf9f53d47322ef5f90f36aa5e83bb5be209e4d
Grace Jiang <gracejiang@dhcp-10-31-91-80.dyn.mit.edu>
2022-01-24 14:53:21 -0500

    styling

--- client/src/components/pages/Oops.js
+++ client/src/components/pages/Oops.js
@@ -1,10 +1,12 @@
 import React from "react";
-import "./NotFound.css";
+import "./Oops.css";
 
 const Oops = () => {
     return (
-        <>
-            <h1>Oops!</h1>
+        <div className="NotFound-container">
+        <div className="NotFound-title">Oops!</div>
+        <div className="NotFound-info">
+            <div className="NotFound-text">
             <p>
                 This page is for ninja eyes only.
             </p>
@@ -14,7 +16,13 @@ const Oops = () => {
             <p>
                 If you are not a ninja, please register to become a novice ninja.
             </p>
-        </>
+            </div>
+            <div className="NotFound-text">
+            Have some fun ninja images for now:
+            </div>
+            <div className="NotFound-image"/>
+        </div>
+        </div>
     );
 };
 
