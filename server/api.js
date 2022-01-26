9ef0dca2699d1004dcebfc37deea3567419ec644
Liane Xu <lianexu@dhcp-10-31-126-90.dyn.mit.edu>
2022-01-25 20:43:11 -0500

    whole numbers to integers

--- server/api.js
+++ server/api.js
@@ -30,61 +30,61 @@ const data = {
       _id: 0,
       function: '1(x+0)^2+0',
       hint: 'Sensei: Welcome to the dojo, Graph-Ninja-in-Training. Here, we will train 3 skills: (a) stretch, (b) sidestep, and (c) levitate. We will start with stretching. Go ahead: Input a=1 and see if you can match my yellow curve.',
-      note: 'Important: input b=0, c=0, and whole numbers only!'
+      note: 'Important: input b=0, c=0, and integers only!'
     },
     {
       _id: 1,
       function: '4(x+0)^2+0',
       hint: 'Sensei: Great! See if you can stretch the graph differently, and match me again. What does a=2 do? a=3? a=4?',
-      note: 'Important: input b=0, c=0, and whole numbers only!'
+      note: 'Important: input b=0, c=0, and integers only!'
     },
     {
       _id: 2,
       function: '-2(x+0)^2+0',
       hint: 'Sensei: Graph ninjas are never caught unaware, so here’s a curveball to keep you on your toes. How will you match this?',
-      note: 'Important: input b=0, c=0, and whole numbers only!'
+      note: 'Important: input b=0, c=0, and integers only!'
     },
     {
       _id: 3,
       function: '1(x+-1)^2+0',
       hint: 'Sensei: Nice one! You’re ready to practice sidestepping – this might be another curveball. Observe what happens when you invoke b=1 and b=-1.',
-      note: 'Important: input a=1, c=0, and whole numbers only!'
+      note: 'Important: input a=1, c=0, and integers only!'
     },
     {
       _id: 4,
       function: '1(x+2)^2+0',
       hint: 'Sensei: Sliiide to the left…Cha cha real smooth.',
-      note: 'Important: input a=1, c=0, and whole numbers only!'
+      note: 'Important: input a=1, c=0, and integers only!'
     },
     {
       _id: 5,
       function: '2(x+4)^2+0',
       hint: 'Sensei: Now that we’ve practiced (a) stretching and (b) sidestepping separately, I want you to combine them.',
-      note: 'Important: input c=0, and whole numbers only!'
+      note: 'Important: input c=0, and integers only!'
     },
     {
       _id: 6,
       function: '1(x+0)^2+3',
       hint: 'Sensei: That wasn’t that hard. Now I’ll allow you to try levitation. Here are some original words of wisdom: It’s leviosa, not leviosa! Anyways, try different values of (c).',
-      note: 'Important: input a=1, b=0, and whole numbers only!'
+      note: 'Important: input a=1, b=0, and integers only!'
     },
     {
       _id: 7,
       function: '1(x+0)^2+-3',
       hint: 'Sensei: What’s the opposite of leviosa? Levios-ain’t. How should you change (c) now?',
-      note: 'Important: input a=1, b=0, and whole numbers only!'
+      note: 'Important: input a=1, b=0, and integers only!'
     },
     {
       _id: 8,
       function: '2(x+1)^2+2',
       hint: 'Sensei: Too easy? Let’s see if you can use all 3 skills together.',
-      note: 'Important: input whole numbers only!'
+      note: 'Important: input integers only!'
     },
     {
       _id: 9,
       function: '4(x+-1)^2+-2',
       hint: 'Sensei: This is my last test. It is now time for me to send you off into the world. Navigate to the Freestyle room in the dojo to discover more Skills you might need in the future. Remember: with great power comes great responsibility.',
-      note: 'Important: input whole numbers only!'
+      note: 'Important: input integers only!'
     },
 
   ],
